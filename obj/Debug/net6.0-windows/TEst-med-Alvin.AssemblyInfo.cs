38d07d0288ad534801b620f9beab3f18a53447bd
ImagineANDIZZZ <Arwi07001@utb.vaxjo.se>
2025-05-28 15:03:51 +0200

    1

--- obj/Debug/net6.0-windows/TEst-med-Alvin.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/TEst-med-Alvin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEst-med-Alvin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97ca5bdb7d2cc06a8d401b90d615612ac8c4955")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ac8ffe0c64888bce16eb505c12a159eaaecb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEst-med-Alvin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEst-med-Alvin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
